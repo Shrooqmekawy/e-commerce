b76956ad23ecac9c87d1de95c827bb433fc5dc73
Shrooqmekawy <shrooqzaki20@gmail.com>
2025-03-01 06:33:29 +0200

    test

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'payment/:id', renderMode: RenderMode.Server },
+  { path: 'details/:id', renderMode: RenderMode.Server },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
